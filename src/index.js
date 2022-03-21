61f8785b0ad402f96bfc6961baaac95da73b0e9f
Maicon Douglas <11251303+mikedpsm@users.noreply.github.com>
2022-03-21 18:41:37 -0300

    Add: Swagger document descriptions
    Added dependencies for better documentation, and descriptions for a few routes. WIP

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const cors = require('cors');
-const express = require('express');
-const router = require('./routes/route');
+const swaggerUI = require("swagger-ui-express");
+const cors = require("cors");
+const express = require("express");
+const router = require("./routes/route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', router);
+app.use("/api", router);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(require("../swagger.json")));
 
 app.listen(process.env.PORT || 5000);
